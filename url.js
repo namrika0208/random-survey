c4d36db9924a229557650baaea2fdbe9c01ef07a
namrika0208 <153161099+namrika0208@users.noreply.github.com>
2023-12-07 16:08:10 +0800

    url.js

--- url.js
+++ url.js
@@ -11,5 +11,13 @@ const urls = [
 	'https://forms.gle/x6VwZj8nKUhUvyEq9',
 	'https://forms.gle/vHHkUHyiPHEH3bRW7',
 	'https://forms.gle/XmpHy2ytG85afNSW9',
-	'hhttps://forms.gle/sQqkTEYu99PKVJyNA'
+	'https://forms.gle/sQqkTEYu99PKVJyNA',
+	'https://forms.gle/Bg8a3B3gzt6iD1JHA',
+	'https://forms.gle/RN2BA5EZ7sHsMeLb8',
+	'https://forms.gle/SqmkZsJadosA2SqaA',
+	'https://forms.gle/qwLwvMTnMzkt8Pw8A',
+	'https://forms.gle/q4ZnNU9Aey8ZCSBB7',
+	'https://forms.gle/WzWvZuoDn7NypdSY9',
+	'https://forms.gle/RZLn4UQQEjc9DeibA',
+	'https://forms.gle/Cfb2C25kMB786tMv6'
 ];
